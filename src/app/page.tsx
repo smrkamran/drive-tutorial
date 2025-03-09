5d2a8518c2a703c1acee748a2988010b1d4ddeb5
Sameer Kamran <sameer.kamran@schwarm.com>
2025-03-09 14:22:21 +0100

    Added new Ui with static data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import Link from "next/link";
+import { DriveUI } from "~/components/drive-ui"
 
-export default function HomePage() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">Hey boyee</div>
-    </main>
-  );
+export default function Home() {
+  return <DriveUI />
 }
+

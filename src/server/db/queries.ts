fe408a6aae58cb4e9791a5759cb0a418f38815f7
Sameer Kamran <sameer.kamran@schwarm.com>
2025-03-16 21:09:58 +0100

    cleanup data access layer

--- src/server/db/queries.ts
+++ src/server/db/queries.ts
@@ -1,41 +1,44 @@
+import "server-only";
+
 import { eq } from "drizzle-orm";
-import DriveContents from "~/components/drive-contents";
 import { db } from "~/server/db";
 import {
   files_table as filesSchema,
   folders_table as foldersSchema,
 } from "~/server/db/schema";
 
-export async function getAllParentsForFolder(folderId: number) {
-  const parents = [];
-  let currentId: number | null = folderId;
+export const QUERIES = {
+  getAllParentsForFolder: async function (folderId: number) {
+    const parents = [];
+    let currentId: number | null = folderId;
 
-  while (currentId !== null) {
-    const folder = await db
-      .selectDistinct()
-      .from(foldersSchema)
-      .where(eq(foldersSchema.id, currentId));
+    while (currentId !== null) {
+      const folder = await db
+        .selectDistinct()
+        .from(foldersSchema)
+        .where(eq(foldersSchema.id, currentId));
 
-    if (!folder[0]) {
-      throw new Error("Parent not found");
+      if (!folder[0]) {
+        throw new Error("Parent not found");
+      }
+
+      parents.unshift(folder[0]);
+      currentId = folder[0]?.parent;
     }
+    return parents;
+  },
 
-    parents.unshift(folder[0]);
-    currentId = folder[0]?.parent;
-  }
-  return parents;
-}
+  getFiles: function (parsedFolderId: number) {
+    return db
+      .select()
+      .from(filesSchema)
+      .where(eq(filesSchema.parent, parsedFolderId));
+  },
 
-export function getFiles(parsedFolderId: number) {
-  return db
-    .select()
-    .from(filesSchema)
-    .where(eq(filesSchema.parent, parsedFolderId));
-}
-
-export function getFolders(parsedFolderId: number) {
-  return db
-    .select()
-    .from(foldersSchema)
-    .where(eq(foldersSchema.parent, parsedFolderId));
-}
+  getFolders: function (parsedFolderId: number) {
+    return db
+      .select()
+      .from(foldersSchema)
+      .where(eq(foldersSchema.parent, parsedFolderId));
+  },
+};

6ac3e73d208563cc784e86df090d32cdfc7ed9a0
Sameer Kamran <sameer.kamran@schwarm.com>
2025-03-16 17:44:18 +0100

    resolved breadcrumbs

--- src/app/f/[folderId]/page.tsx
+++ src/app/f/[folderId]/page.tsx
@@ -6,6 +6,26 @@ import {
   folders as foldersSchema,
 } from "~/server/db/schema";
 
+async function getAllParents(folderId: number) {
+  const parents = [];
+  let currentId: number | null = folderId;
+
+  while (currentId !== null) {
+    const folder = await db
+      .selectDistinct()
+      .from(foldersSchema)
+      .where(eq(foldersSchema.id, currentId));
+
+    if (!folder[0]) {
+      throw new Error("Parent not found");
+    }
+
+    parents.unshift(folder[0]);
+    currentId = folder[0]?.parent;
+  }
+  return parents;
+}
+
 export default async function GoogleDriveClone(props: {
   params: Promise<{ folderId: string }>;
 }) {
@@ -15,13 +35,21 @@ export default async function GoogleDriveClone(props: {
     return <div>Invalid folder id</div>;
   }
 
-  const files = await db
+  const filesPromise = db
     .select()
     .from(filesSchema)
     .where(eq(filesSchema.parent, parsedFolderId));
-  const folders = await db
+  const foldersPromise = db
     .select()
     .from(foldersSchema)
     .where(eq(foldersSchema.parent, parsedFolderId));
-  return <DriveContents files={files} folders={folders} />;
+  const parentsPromise = getAllParents(parsedFolderId);
+
+  const [files, folders, parents] = await Promise.all([
+    filesPromise,
+    foldersPromise,
+    parentsPromise,
+  ]);
+
+  return <DriveContents files={files} folders={folders} parents={parents} />;
 }
